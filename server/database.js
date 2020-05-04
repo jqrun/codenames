451449d5e356dfb88c165e2b4ecf8dcb7311bd8c
jqdotrun <hello@jq.run>
2020-05-03 23:50:58 -0400

    Update to dotenv for firebase configss

--- server/database.js
+++ server/database.js
@@ -1,15 +1,16 @@
 const firebase = require('firebase');
 require('firebase/firestore');
+require('dotenv').config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAEEz2hilZoy1niHMU4Bi3m6LouZSkknM",
-  authDomain: "codenames-273814.firebaseapp.com",
-  databaseURL: "https://codenames-273814.firebaseio.com",
-  projectId: "codenames-273814",
-  storageBucket: "codenames-273814.appspot.com",
-  messagingSenderId: "825324062075",
-  appId: "1:825324062075:web:46e39a5de955e038a669de",
-  measurementId: "G-K289QC9ZQM"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 class Database {
